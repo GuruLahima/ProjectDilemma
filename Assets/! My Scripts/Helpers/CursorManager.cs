42852e9f046d60609651565aa9ca2bdc07eedf80
Aleksandar Ruseski <name_not@live.com>
2022-03-03 10:35:30 +0100

    cursor management script

---
+++ Assets/! My Scripts/Helpers/CursorManager.cs
@@ -0,0 +1,72 @@
+using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+
+  [BoxGroup("OnStart")]
+  [SerializeField] bool toggleCursorOnStart;
+  [BoxGroup("OnStart")]
+  [SerializeField] bool setCursorOnStart;
+  [ShowIf("setCursorOnStart")]
+  [BoxGroup("OnStart")]
+  [SerializeField] bool visibilityOnStart;
+  [ShowIf("setCursorOnStart")]
+  [BoxGroup("OnStart")]
+  [SerializeField] CursorLockMode lockModeOnStart;
+  [BoxGroup("OnDisable")]
+  [SerializeField] bool toggleCursorOnDisable;
+  [BoxGroup("OnDisable")]
+  [SerializeField] bool setCursorOnDisable;
+  [BoxGroup("OnDisable")]
+  [ShowIf("setCursorOnDisable")]
+  [SerializeField] bool visibilityOnDisable;
+  [BoxGroup("OnDisable")]
+  [ShowIf("setCursorOnDisable")]
+  [SerializeField] CursorLockMode lockModeOnDisable;
+
+  private void Start()
+  {
+    if (toggleCursorOnStart)
+    {
+      ToggleLockMode();
+      ToggleVisibility();
+    }
+    if (setCursorOnStart)
+    {
+      SetLockMode(lockModeOnStart);
+      SetVisibility(visibilityOnStart);
+    }
+  }
+  private void OnDisable()
+  {
+    if (toggleCursorOnDisable)
+    {
+      ToggleLockMode();
+      ToggleVisibility();
+    }
+    if (setCursorOnDisable)
+    {
+      SetLockMode(lockModeOnDisable);
+      SetVisibility(visibilityOnDisable);
+    }
+  }
+
+  public void ToggleLockMode()
+  {
+    Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+  }
+  public void ToggleVisibility()
+  {
+    Cursor.visible = !Cursor.visible;
+  }
+  public void SetLockMode(CursorLockMode mode)
+  {
+    Cursor.lockState = mode;
+  }
+  public void SetVisibility(bool visible)
+  {
+    Cursor.visible = visible;
+  }
+}
